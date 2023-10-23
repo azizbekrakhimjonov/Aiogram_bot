18231e2ffbc9477c243ed1cf0c88cc182aa93d78
azizbekrahimjonov <azizbekrahimjonov571@gmail.com>
2023-10-23 17:58:56 +0500

    add buttons.py

--- buttons.py
+++ buttons.py
@@ -1,6 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-r_btn = KeyboardButton('Random Number')
-menu = ReplyKeyboardMarkup(resize_keyboard=True).add(r_btn)
+btn1 = KeyboardButton('Python')
+btn2 = KeyboardButton('Java')
+btn3 = KeyboardButton('JavaScript')
+btn4 = KeyboardButton('C++')
+btn5 = KeyboardButton('C#')
+menu = (ReplyKeyboardMarkup(resize_keyboard=True)
+        .add(btn1, btn2).add(btn3).add(btn4, btn5))
 
 
